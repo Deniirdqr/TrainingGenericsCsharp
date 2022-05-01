4da5531d8c039c85f766047e0f005df89b8b5790
perkles <deniordqr@hotmail.com>
2022-04-30 21:12:57 -0300

    Add project files.

---
+++ TrainingSixteen/PrintService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TrainingSixteen
+{
+    class PrintService<T>
+    {
+        private T[] Values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            Values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if(_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return Values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int x = 0; x < _count - 1; x++)
+            {
+                Console.Write(Values[x] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(Values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ TrainingSixteen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TrainingSixteen;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<string> printService = new PrintService<string>();
+
+            Console.Write("How many values? ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                string _value = Console.ReadLine();
+                printService.AddValue(_value);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
